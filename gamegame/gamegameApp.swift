1b24e23eed6a20ccd6692cdde53df733949e0a7e
keshew <artyom.korotckov2017@yandex.ru>
2026-01-18 17:27:16 +0500

    Initial Commit

---
+++ gamegame/gamegameApp.swift
@@ -0,0 +1,17 @@
+//
+//  gamegameApp.swift
+//  gamegame
+//
+//  Created by Артём Коротков on 18.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct gamegameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
